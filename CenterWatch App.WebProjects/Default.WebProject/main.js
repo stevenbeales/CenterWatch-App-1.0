0820b9e8d1429e21aaf49bc44be7b59841abe78f
Steven Beales <stevenbeales@gmail.com>
2016-07-15 15:24:39 -0400

    Trial Details updates

--- CenterWatch App.WebProjects/Default.WebProject/main.js
+++ CenterWatch App.WebProjects/Default.WebProject/main.js
@@ -1,38 +1,38 @@
 var CWApp = (function () {
 
-    var _isNumeric = function(n) {
+    var _isNumeric = function (n) {
 		return !isNaN(parseFloat(n)) && isFinite(n);
 	};
-	
-	var abbreviate = function(str, max, suffix) {
+
+	var abbreviate = function (str, max, suffix) {
 		if (str.length === 0) {
 			return '';
-		}		
+		}
 
-		if((str = str.replace(/^\s+|\s+$/g, '').replace(/[\r\n]*\s*[\r\n]+/g, ' ').replace(/[ \t]+/g, ' ')).length <= max) {
+		if ((str = str.replace(/^\s+|\s+$/g, '').replace(/[\r\n]*\s*[\r\n]+/g, ' ').replace(/[ \t]+/g, ' ')).length <= max) {
 			return str;
-		} 
+		}
 
 		var abbr = '';
 		str = str.split(' ');
 		suffix = (typeof suffix !== 'undefined' ? suffix : ' ...');
 		max -= suffix.length;
-	  
-		for(var len = str.length, i = 0; i < len; i++) {
-			if((abbr + str[i]).length < max) {
-			  abbr += str[i] + ' ';
+
+		for (var len = str.length, i = 0; i < len; i++) {
+			if ((abbr + str[i]).length < max) {
+				abbr += str[i] + ' ';
 			}
-			else { 
-				break; 
+			else {
+				break;
 			}
 		}
 		return abbr.replace(/[ ]$/g, '') + suffix;
 	};
-			
-	var addSiteMarkers = function(mapObj, lObj, groupName) { 			
+
+	var addSiteMarkers = function (mapObj, lObj, groupName) {
 		mapObj.removeMarkerGroup(groupName);
 		var settings = {
-			group: groupName, 
+			group: groupName,
 			icon: {
 				anchor: {
 				}
@@ -62,7 +62,7 @@ var CWApp = (function () {
 		var arr = [];
 		var lat, lon, city, state;
 		var _d = lObj._data;
-		for(var len = _d.length, i = 0; i < len; i++) { 
+		for (var len = _d.length, i = 0; i < len; i++) {
 			delete arr;
 			arr = [];
 			lat = _d[i].latitude;
@@ -73,24 +73,20 @@ var CWApp = (function () {
 				arr.push(lat);
 				arr.push(lon);
 				settings.title = city + ', ' + state;
-				mapObj.addMarker('MYMARKER_' + i,arr,settings);
+				mapObj.addMarker('MYMARKER_' + i, arr, settings);
 			}
 		}
 		mapObj.viewMarkerGroups(groupName);
 	};
-	
-	var goBackToMainMenu = function(ux) {
+
+	var goBackToMainMenu = function (ux) {
 		var parentObject = ux.getParentObject();
 		var mainPanel = parentObject.panelSetActive('PANEL_MAIN', true);
 	};
 
- 	return {
+	return {
 		abbreviate: abbreviate,
 		addSiteMarkers: addSiteMarkers,
 		goBackToMainMenu: goBackToMainMenu
 	};
 })();
-
-
-
-
